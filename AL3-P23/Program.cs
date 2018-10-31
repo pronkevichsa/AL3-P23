1df78d0c2dbfb9e034de894c9c5c8cd31d158797
Sergei <ps_minsk@mail.ru>
2018-10-31 23:37:49 +0300

    Add project files.

---
+++ AL3-P23/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL3_P23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
